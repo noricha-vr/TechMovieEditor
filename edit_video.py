028bf83c7b90e060008bc8aacd523e388e4e477b
noricha-vr <noricha.vr@gmail.com>
2024-03-16 22:44:26 +0900

    Add comments

--- edit_video.py
+++ edit_video.py
@@ -33,35 +33,45 @@ if __name__ == "__main__":
     target_fps = 60
     target_audio_rate = 48000
 
+    # 入力動画をフォーマットする
     input_formatter = VideoFormatter(
         input_video, tmp_dir, start_time, end_time, target_resolution, target_fps, target_audio_rate)
     formatted_input_video = input_formatter.format_video()
 
+    # オープニング動画をフォーマットする
     opening_formatter = VideoFormatter(opening_video, tmp_dir, target_resolution=target_resolution,
                                        target_fps=target_fps, target_audio_rate=target_audio_rate)
     formatted_opening_video = opening_formatter.format_video()
 
+    # エンディング動画をフォーマットする
     ending_formatter = VideoFormatter(ending_video, tmp_dir, target_resolution=target_resolution,
                                       target_fps=target_fps, target_audio_rate=target_audio_rate)
     formatted_ending_video = ending_formatter.format_video()
 
+    # フォーマットされた動画を読み込む
     main_video = VideoFileClip(formatted_input_video)
     opening = VideoFileClip(formatted_opening_video)
     ending = VideoFileClip(formatted_ending_video)
 
+    # 無音部分を削除する
     silence_remover = SilenceRemover(main_video, silence_threshold, chunk_size)
     final_main_video = silence_remover.remove_silent_parts()
 
+    # オープニングとメイン動画のクロスフェードを作成する
     opening_main = CrossfadeTransition.create_crossfade_transition(
         opening, final_main_video.subclip(0, crossfade_duration), crossfade_duration)
 
+    # メイン動画とエンディングのクロスフェードを作成する
     main_ending = CrossfadeTransition.create_crossfade_transition(final_main_video.subclip(
         final_main_video.duration - crossfade_duration), ending, crossfade_duration)
 
+    # クロスフェードを除いたメイン動画の部分を取得する
     main_video_without_crossfade = final_main_video.subclip(
         crossfade_duration, final_main_video.duration - crossfade_duration)
+    # 最終的な動画を結合する
     final_video = concatenate_videoclips(
         [opening_main, main_video_without_crossfade, main_ending]).set_fps(24000/1001)
+    # 動画をファイルに書き出す
     final_video.write_videofile(
         output_video,
         codec="libx264",
